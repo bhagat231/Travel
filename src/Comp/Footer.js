3455bb82f054d04adfcc9164c494cd307ec0e1ed
bhagat231 <sushantbhagat000@gmail.com>
2022-11-30 12:52:07 +0545

    my fourth commit

---
+++ src/Comp/Footer.js
@@ -0,0 +1,103 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+    <div className='container-fluid foot py-3 '>
+        <div className='row '>
+          <div className='col-lg-12 '>
+            <div className='row'>
+            <h3 className='text-black fw-bolder  text-center'>We're affiliated & Join with:</h3>
+            <div>
+                <div className='ms-5 d-flex mt-3 d-flex justify-content-center    '>
+                    <img className='mb-5 fw-bolder'  width="75" src='https://www.nepaltourstravel.com/themes/images/svg/associated/nepal-goverment.svg'/>
+                    <p className='text-dark fw-bolder mx-1 lh-1'>Registered with: Company<br/> Registration Office,<br/> Government of Nepal</p>
+                    <img className='ms-5 mb-5 fw-bolder'  width="75" src='https://www.nepaltourstravel.com/themes/images/svg/associated/ntb.svg'/>
+                    <p className='text-dark fw-bolder mx-1 lh-1'>Licensed by: Nepal Tourism<br/> Board & Department of<br/> Tourism</p>
+                    <img className='ms-5 mb-5 fw-bolder'  width="75" src='https://www.nepaltourstravel.com/themes/images/svg/associated/taan.svg'/>
+                    <p className='text-dark fw-bolder mx-1 lh-1'>Proud Member of Trekking<br/> Agency's Association of Nepal</p>
+                    <img className='ms-5 mb-5 fw-bolder'  width="75" src='https://www.nepaltourstravel.com/themes/images/svg/associated/nma.svg'/>
+                    <p className='text-dark fw-bolder mx-1 lh-1'>General Member of Nepal<br/> Mountaineering<br/>Association</p>
+                </div>
+                
+            </div>
+            </div>
+            </div>
+           
+
+
+
+        </div>
+        <div className='container py-3 '>
+      <div className='row text-dark fw-bolder  text-center border-bottom '>
+        <div className=' col-lg-12 d-flex justify-content-center '>
+          <h5 className='fs-1 mt-4'><i class="bi bi-telephone "></i></h5>
+          <div className=' lh-1 mt-4  '>
+        <p className='ms-0'>Call Us</p>
+        <p>+977 9851035823</p>
+        </div>
+        <div className='kaun d-flex  pe-5'>
+        <h5 className='fs-2 '><i class="bi bi-envelope-exclamation"></i></h5>
+        <div className='lh-1 '>
+        <p>Email for us</p>
+        <p>welcomenepaltreks@gmail.com</p>
+        </div>
+        </div>
+        <div className='kaun d-flex mb-4'>
+        <h5 className='fs-2 '><i class="bi bi-brightness-high"></i></h5>
+        <div className='lh-1 ms-3'>
+        <p className='lh-1'>Follow us</p>
+        <p><i class="bi bi-facebook "></i><span className='mx-2'><i class="bi bi-facebook"></i></span><span><i class="bi bi-facebook"></i></span></p>
+       
+        </div>
+        </div>
+         </div>
+      </div>
+      <div className='container py-3'>
+      <div className='row text-dark fw-bolder lh-1'>
+        <div className='col-lg-4 text-center'>
+          <h5>Home</h5>
+          <p>Nepal Budget Trek</p>
+          <p>Travel Info</p>
+          <p>Videos</p>
+        </div>
+        <div className='col-lg-4 text-center'>
+          <h5>Company Info</h5>
+          <p>Nepal Travel Package</p>
+          <p>Read Reviews</p>
+          <p>Payment and Booking</p>
+        </div>
+        <div className='col-lg-4 text-center'>
+          <h5>Nepal Luxury Tours Package</h5>
+         <div>
+          <p>Destinations</p>
+          <p>Photos</p>
+          <p>FAQs</p>
+          </div>
+        </div>
+      </div>
+    </div>
+      
+    </div>
+    </div>
+    <div className='container kbc py-3 '>
+      <div className='row text-dark fw-bolder'>
+        <div className='col-lg-4  d-flex align-items-center'>
+          <h5 className='px-2'>Secure Payment</h5>
+          <img  width="35" src='https://www.nepaltourstravel.com/themes/images/svg/payment/visa.svg'/>
+          <img className='mx-2' width="35"  src='https://www.nepaltourstravel.com/themes/images/svg/payment/amex.svg'/>
+          <img  width="35" src='https://www.nepaltourstravel.com/themes/images/svg/payment/mastercard.svg'/>
+        </div>
+        <div className='col-lg-4 py-3 text-center '>
+          <h5>© 2022, Welcome Nepal Treks and Tours Pvt. Ltd.  |</h5>
+        </div>
+        <div className='col-lg-4 py-3 text-center  '>
+          <h5>All Rights Reserved Subscribe to Newsletter</h5>
+           <h5>Designed by Imagine Web Solution</h5>
+        </div>
+      </div>
+    </div>
+    
+    </>
+  )
+}
